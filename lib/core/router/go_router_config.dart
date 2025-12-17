fef6a43fc7d2dd30520cac6ede448a5585d991e1
momaher74 <elomdamohamedmaher@gmail.com>
2025-12-17 18:47:21 +0200

    side menu

--- lib/core/router/go_router_config.dart
+++ lib/core/router/go_router_config.dart
@@ -70,7 +70,7 @@ final GoRouter router = GoRouter(
       path: splashView,
       name: splashView,
       pageBuilder: (context, state) =>
-          _fadeSlidePage(state: state, child: const SettingView()),
+          _fadeSlidePage(state: state, child: const SplashView()),
     ),
 
     /// Layout
